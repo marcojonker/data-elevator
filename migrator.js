d015b6e999107b5453a23f0305b8f33ddfff0d4f
Marco <mj.jonker@live.nl>
2016-05-11 09:58:58 +0200

    Init migrations

---
+++ migrator.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Migrator
+ * Processor for migrations
+ * 
+**/
+
+'use strict'
+
+var MigratorBase = require('./lib/migrator-base.js');
+var util = require('util');
+
+/**
+ * Constructor
+ */
+var Migrator = function() {
+    Migrator.super_.apply(this, arguments);
+};
+
+util.inherits(Migrator, MigratorBase);
+
+var migrator = new Migrator();
+
+migrator.run(function(error) {
+    if(error) {
+        console.log(error.message);
+    }
+})
+
