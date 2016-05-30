1dad99c52e0b7ecaa20cc07cb21ff841f8e90e22
Marco <mj.jonker@live.nl>
2016-05-30 08:59:30 +0200

    renaming to elevator

--- elevator.js
+++ elevator.js
@@ -1,30 +1,29 @@
 /**
- * Migrator
- * Processor for migrations
+ * Elevator
+ * Data elevator
  * 
 **/
 
 'use strict'
 
+var util = require('util');
 var ElevatorBase = require('./lib/elevator-base.js');
 var ConsoleLogger = require('./lib/logger/console-logger.js');
-
-var util = require('util');
 
 /**
  * Constructor
  */
-var Elevator = function() {
+var Elevator = function(logger, LevelController) {
     Elevator.super_.apply(this, arguments);
 };
 
 util.inherits(Elevator, ElevatorBase);
 
-var elevator = new ElevatorBase(new ConsoleLogger(false));
+var elevator = new Elevator(new ConsoleLogger(false));
 
 elevator.run(function(error) {
     if(error) {
         console.log(error.message);
     }
-})
+});
 
